3c5b3077f261510212c8f3e25be3d7d7629344df
kobarei <koba.reiji@gmail.com>
2013-10-25 19:28:41 +0900

    remove initialize method

--- controle_ec2.rb
+++ controle_ec2.rb
@@ -2,20 +2,16 @@ class ControleEc2
   require 'aws-sdk'
   require 'yaml'
 
-  def initialize(instance_id)
-    @instance_id = instance_id
-  end
-
   def setup(region)
     AWS.config(YAML.load(File.read("config.yml")))
     AWS.config(region: region)
   end
 
   %w(start stop).each do |name|
-    define_method name do
+    define_method name do |instance_id|
       begin
         puts "Trying to #{name} EC2..."
-        instance = AWS::EC2.new.instances[@instance_id]
+        instance = AWS::EC2.new.instances[instance_id]
         instance.send(name)
       rescue => e
         puts "Failed to #{name}."
@@ -27,6 +23,6 @@ class ControleEc2
 
 end
 
-ec2 = ControleEc2.new(ARGV[1])
+ec2 = ControleEc2.new
 ec2.setup(ARGV[0])
-ec2.send(ARGV[2])
+ec2.send(ARGV[2], ARGV[1])
